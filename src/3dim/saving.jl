acdb5d68fa2ca303a8aa54e72c820b347e350582
Mateusz Kapusta <mitra@Voyager-2.local>
2025-06-15 10:09:01 +0200

    Fixed parallel dumps with h5

--- src/3dim/saving.jl
+++ src/3dim/saving.jl
@@ -35,53 +35,73 @@ function SaveHDF5Gather(comm,P::VeronaArr{T},XMPI::Int64,YMPI::Int64,ZMPI::Int64
         end
     end
 end
-
 function SaveHDF5Parallel(comm, P::VeronaArr{T}, XMPI::Int, YMPI::Int, ZMPI::Int, name::String, to_save::Dict) where {T <: Real}
     rank = MPI.Comm_rank(comm)
     local_data = @view P.arr[:, 4:end-3, 4:end-3, 4:end-3]
     local_data = Array(local_data)  # ensure memory is contiguous
-
+    
     global_size_X = XMPI * (P.size_X - 6)
     global_size_Y = YMPI * (P.size_Y - 6)
     global_size_Z = ZMPI * (P.size_Z - 6)
-
-    px,py,pz = MPI.Cart_coords(comm) #compute given coordinates
-
+    
+    px, py, pz = MPI.Cart_coords(comm)  # compute given coordinates
     offset_x = px * (P.size_X - 6)
     offset_y = py * (P.size_Y - 6)
     offset_z = pz * (P.size_Z - 6)
-
+    
     local_size = size(local_data)
-
+    
+    # Create file access property list for MPI-IO
     fapl = HDF5.h5p_create(HDF5.H5P_FILE_ACCESS)
     HDF5.h5p_set_fapl_mpio(fapl, comm, MPI.INFO_NULL)
-
+    
+    # Create/open file
     fid = HDF5.h5f_create(name, HDF5.H5F_ACC_TRUNC, HDF5.H5P_DEFAULT, fapl)
+    
+    # Define global dimensions
+    global_dims = Vector{HDF5.hsize_t}([5, global_size_X, global_size_Y, global_size_Z])
+    
+    # Create filespace
+    filespace = HDF5.h5s_create_simple(4, global_dims, global_dims)
+    
+    # Create dataset
+    dset = HDF5.h5d_create(fid, "data", HDF5.H5T_NATIVE_DOUBLE, filespace, HDF5.H5P_DEFAULT, HDF5.H5P_DEFAULT, HDF5.H5P_DEFAULT)
 
-    global_dims = (5, global_size_X, global_size_Y, global_size_Z)
-    filespace = HDF5.h5s_create_simple(rank,global_dims)
-
-    # Select local hyperslab in the global file
-    offset = (0, offset_x, offset_y, offset_z)
-    count  = size(local_data)
-    HDF5.h5s_select_hyperslab(filespace, HDF5.H5S_SELECT_SET, offset, nothing, count, nothing)
-
-    memspace = HDF5.h5s_create_simple(count)
-
-    dset = HDF5.h5d_create(fid, "data", HDF5.H5T_NATIVE_DOUBLE, filespace)
-
+    # Define hyperslab for this process
+    offset = Vector{HDF5.hsize_t}([0, offset_x, offset_y, offset_z])
+    block = Vector{HDF5.hsize_t}([5, P.size_X-6, P.size_Y-6, P.size_Z-6])
+    count = Vector{HDF5.hsize_t}([1, 1, 1, 1])
+    stride = Vector{HDF5.hsize_t}([1, 1, 1, 1])
+    
+    # Select hyperslab in filespace
+    HDF5.h5s_select_hyperslab(filespace, HDF5.H5S_SELECT_SET, offset, stride, count, block)
+    
+    # Create memory space matching local data dimensions
+    local_dims = Vector{HDF5.hsize_t}(collect(size(local_data)))
+    memspace = HDF5.h5s_create_simple(length(local_dims), local_dims, local_dims)
+    
+    # Create data transfer property list for collective I/O
     xfer_plist = HDF5.h5p_create(HDF5.H5P_DATASET_XFER)
     HDF5.h5p_set_dxpl_mpio(xfer_plist, HDF5.H5FD_MPIO_COLLECTIVE)
-
+    
+    # Write data
     HDF5.h5d_write(dset, HDF5.H5T_NATIVE_DOUBLE, memspace, filespace, xfer_plist, local_data)
-
+    
+    # Close spaces and property lists
+    HDF5.h5s_close(memspace)
+    HDF5.h5s_close(filespace)
+    HDF5.h5p_close(xfer_plist)
+    HDF5.h5d_close(dset)
+    
+    # Save additional metadata (only rank 0)
     if rank == 0
         for (key, val) in to_save
             HDF5.h5write(fid, key, val)
         end
     end
-
-    HDF5.h5d_close(dset)
+    
+    # Close file and property list
     HDF5.h5f_close(fid)
+    HDF5.h5p_close(fapl)
 end
 
