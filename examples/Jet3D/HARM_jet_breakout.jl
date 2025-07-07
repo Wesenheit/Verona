0b37faed0836d8db7f524b6d143d8ba81c7aa9b8
Mateusz Kapusta <plgmrkapusta@login01.athena.cyfronet.pl>
2025-07-07 18:36:22 +0200

    fix examples

--- examples/Jet3D/HARM_jet_breakout.jl
+++ examples/Jet3D/HARM_jet_breakout.jl
@@ -1,22 +1,33 @@
-using BenchmarkTools
 using MPI
 MPI.Init()
+
+using Base.Threads
 using CUDA
 using Verona
+using Random
 
 Type = Float64
 
 @assert MPI.has_cuda()
 comm = MPI.COMM_WORLD
+
+rank = MPI.Comm_rank(comm)
+
+if CUDA.functional()
+    CUDA.device!(rank)
+    @info "Process $rank using GPU $(CUDA.device())"
+end
+
 MPI_X = 1
 MPI_Y = 1
 MPI_Z = 1
+@assert MPI_X * MPI_Y * MPI_Z == MPI.Comm_size(comm)
 comm = MPI.Cart_create(comm,(MPI_X,MPI_Y,MPI_Z), periodic=(false,false,false),reorder = true)
 
 eos = Verona.EosTypes.Polytrope{Type}(4.0/3.0)
 Nx = 512 - 6
-Ny = 256 - 6
-Nz = 256 - 6
+Ny = 512 - 6
+Nz = 512 - 6
 
 P = Verona3D.ParVector3D{Type}(Nx,Ny,Nz)
 tot_X = MPI_X * Nx + 6
@@ -27,7 +38,7 @@ idx,idy,idz=  MPI.Cart_coords(comm)
 
 #one unit -> 10^8cm
 
-floor::Type = 1e-12
+floor::Type = 1e-8
 outer::Type = 1e-4
 box_X::Type = 480.
 box_Y::Type = 120.
@@ -43,56 +54,71 @@ dy::Type = 2*box_Y/tot_Y
 dz::Type = 2*box_Z/tot_Z
 Gamma0::Type = 10.
 
-for i in 1:P.size_X
-    for j in 1:P.size_Y
-        for k in 1:P.size_Z
-            i_g = Verona.local_to_global(i,idx,P.size_X,MPI_X)
-            j_g = Verona.local_to_global(j,idy,P.size_Y,MPI_Y)
-            k_g = Verona.local_to_global(k,idz,P.size_Z,MPI_Z)
-            if i_g == 0 || j_g == 0 || k_g == 0
-                continue
-            end
-            X = i_g * dx
-            Y = j_g * dy - box_Y
-            Z = k_g * dz - box_Z
+if rank == 0
+    println("dx: ",dx)
+    println("dy: ",dy)
+    println("dz: ",dz)
+end
+seed = 42
 
-            R = sqrt(X^2 + Y^2 + Z^2)
-            if R < R_max
-                if R < R_eng
-                    rho = Rho0 * (R_max/R_eng)^2 * (1+randn()*3e-2)
-                else
-                    rho = Rho0 * (R_max/R)^2 * (1+randn()*3e-2)
-                end
-            else
-                rho = outer
-            end
-            P.arr[1,i,j,k] = rho
-            P.arr[2,i,j,k] = rho*Temp
+const thread_rngs = [MersenneTwister(seed + i) for i in 1:Threads.nthreads()]
+start_calc = time()
 
+Threads.@threads for num in 1:(P.size_X * P.size_Y * P.size_Z)
+
+    cart_idx = CartesianIndices((P.size_X,P.size_Y,P.size_Z))[num]
+    i,j,k = Tuple(cart_idx)
+    i_g = Verona.local_to_global(i,idx,P.size_X,MPI_X)
+    j_g = Verona.local_to_global(j,idy,P.size_Y,MPI_Y)
+    k_g = Verona.local_to_global(k,idz,P.size_Z,MPI_Z)
+   
+    if i_g == 0 || j_g == 0 || k_g == 0
+        continue
+    end
+    X = i_g * dx
+    Y = j_g * dy - box_Y
+    Z = k_g * dz - box_Z
+
+    @fastmath begin 
+        R = sqrt(X^2 + Y^2 + Z^2)
+        if R < R_max
             if R < R_eng
-                v = 1/(1-1/Gamma0^2) * 1/cosh( sqrt(X^2+Y^2) /R_eng)^8
-                ux = Gamma0 * v
-                uy = 0
-                uz = 0
-                P.arr[1,i,j,k] = rho*1e-2
-                P.arr[2,i,j,k] = P.arr[1,i,j,k]*1e-5
+                rho = Rho0 * (R_max/R_eng)^2 * (1+randn(thread_rngs[Threads.threadid()])*3e-2)
             else
-                ux = 0
-                uy = 0
-                uz = 0
+                rho = Rho0 * (R_max/R)^2 * (1+randn(thread_rngs[Threads.threadid()])*3e-2)
             end
-            P.arr[3,i,j,k] = ux
-            P.arr[4,i,j,k] = uy
-            P.arr[5,i,j,k] = uz
+        else
+            rho = outer
         end
+        P.arr[1,i,j,k] = rho
+        P.arr[2,i,j,k] = rho*Temp
+
+        if R < R_eng
+            v = 1/(1-1/Gamma0^2) * 1/cosh( sqrt(X^2+Y^2) /R_eng)^8
+            ux = Gamma0 * v
+            uy = 0
+            uz = 0
+            P.arr[1,i,j,k] = rho*1e-2
+            P.arr[2,i,j,k] = P.arr[1,i,j,k]*1e-5
+        else
+            ux = 0
+            uy = 0
+            uz = 0
+        end
+        P.arr[3,i,j,k] = ux
+        P.arr[4,i,j,k] = uy
+        P.arr[5,i,j,k] = uz
     end
 end
-Cmax::Type = 0.8
+end_calc = time()
+
+
+Cmax::Type = 0.4
 dt::Type = Cmax /(1/dx + 1/dy + 1/dz)
 T::Type = box_X
 n_it::Int64 = 50.
 tol::Type = 1e-6
-reconstruction_method = Val(:MINMOD)
+reconstruction_method = Val(:PPM)
 T_exp::Type = box_X/10
 
 
@@ -127,12 +153,14 @@ end
 
 if MPI.Comm_rank(comm) == 0
     println("dt: ",dt)
+    println("grid setup ", end_calc-start_calc)
+    println("Threads: ",nthreads())
 end
-drops::Type = T/100.
+drops::Type = T/1000.
 SizeX = 4
 SizeY = 4
 SizeZ = 4
 CuP = Verona3D.CuParVector3D{Type}(P)
-CUDA.@time Verona3D.HARM_HLL(comm,CuP,MPI_X,MPI_Y,MPI_Z,SizeX,SizeY,SizeZ,dt,dx,dy,dz,T,eos,drops,floor,ARGS[1],n_it,tol,TurnOff,reconstruction_method)
+Verona3D.HARM_HLL(comm,CuP,MPI_X,MPI_Y,MPI_Z,SizeX,SizeY,SizeZ,dt,dx,dy,dz,T,eos,drops,floor,ARGS[1],n_it,tol,TurnOff,reconstruction_method)
 
 MPI.Finalize()
